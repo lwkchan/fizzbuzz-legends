9ab5ec3f74379883ef8846a02b9d59f33fc99c5e
Laura Chan <laurachan205@gmail.com>
2018-02-20 12:32:33 +0000

    Finish fizzbuzz

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -8,7 +8,67 @@ describe('FizzBuzz', function() {
 
       fizzbuzz = new FizzBuzz();
       expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
+      expect(fizzbuzz.isDivisibleByThree(4)).toBe(false);
+      expect(fizzbuzz.isDivisibleByThree(6)).toBe(true);
+      expect(fizzbuzz.isDivisibleByThree(32)).toBe(false);
     });
+
+    it('divisible by 5', function() {
+
+      fizzbuzz = new FizzBuzz();
+      expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);
+      expect(fizzbuzz.isDivisibleByFive(10)).toBe(true);
+      expect(fizzbuzz.isDivisibleByFive(15)).toBe(true);
+      expect(fizzbuzz.isDivisibleByFive(9)).toBe(false);
+    });
+
+    it('divisible by 15', function(){
+
+      fizzbuzz = new FizzBuzz();
+      expect(fizzbuzz.isDivisibleByFifteen(5)).toBe(false);
+      expect(fizzbuzz.isDivisibleByFifteen(10)).toBe(false);
+      expect(fizzbuzz.isDivisibleByFifteen(15)).toBe(true);
+      expect(fizzbuzz.isDivisibleByFifteen(30)).toBe(true);
+    });
+
+  });
+
+  describe('returns fizz/buzz/fizzbuzz on multiples of 3/5/15 respectively', function() {
+
+    it ('returns fizz on multiples of 3', function(){
+
+      fizzbuzz = new FizzBuzz();
+      expect(fizzbuzz.play(3)).toEqual('fizz');
+      expect(fizzbuzz.play(6)).toEqual('fizz');
+      expect(fizzbuzz.play(9)).toEqual('fizz');
+    });
+
+    it ('returns buzz on multiples of 5', function(){
+
+      fizzbuzz = new FizzBuzz();
+      expect(fizzbuzz.play(5)).toEqual('buzz');
+      expect(fizzbuzz.play(10)).toEqual('buzz');
+      expect(fizzbuzz.play(20)).toEqual('buzz');
+    });
+
+    it ('returns fizzbuzz on multiples of 15', function(){
+
+      fizzbuzz = new FizzBuzz();
+      expect(fizzbuzz.play(15)).toEqual('fizzbuzz');
+      expect(fizzbuzz.play(30)).toEqual('fizzbuzz');
+      expect(fizzbuzz.play(105)).toEqual('fizzbuzz');
+    });
+
+    it ('returns the number if not a multiple of 3 or 5', function(){
+
+      fizzbuzz = new FizzBuzz();
+      expect(fizzbuzz.play(2)).toEqual(2);
+      expect(fizzbuzz.play(4)).toEqual(4);
+      expect(fizzbuzz.play(86)).toEqual(86);
+    });
+
+
+
 
   });
 
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,5 +1,31 @@
 var FizzBuzz = function(){};
 
-FizzBuzz.prototype.isDivisibleByThree = function ( ) {
-  return true
+FizzBuzz.prototype.isDivisibleByThree = function (number) {
+  return number % 3 == 0;
 };
+
+FizzBuzz.prototype.isDivisibleByFive = function (number) {
+  return number % 5 == 0;
+};
+
+FizzBuzz.prototype.isDivisibleByFifteen = function (number) {
+  return number % 15 == 0;
+};
+
+FizzBuzz.prototype.play = function (number) {
+  if (this.isDivisibleByFifteen(number)) {
+  return 'fizzbuzz';
+} else if (this.isDivisibleByFive(number)) {
+  return 'buzz';
+} else if (this.isDivisibleByThree(number)) {
+  return 'fizz';
+} else {
+  return number
+}
+};
+
+var fizzbuzz = new FizzBuzz();
+
+for (var i = 1; i <= 100; i++){
+  console.log(fizzbuzz.play(i))
+}
